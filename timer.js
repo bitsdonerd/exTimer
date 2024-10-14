4db06377e4d08acc3e753ab1ca74f40a093fc848
Lukas <lukasousa70@gmail.com>
2024-10-14 18:57:19 -0300

    Code update

--- timer.js
+++ timer.js
@@ -1,42 +1,52 @@
+// Função global 
+function clock(){
+
 // Função para inicializar as horas corretas
-function createSeconds (seconds) {
-    const data = new Date (seconds * 1000); // A engine do JS sempre conta em Milisegundos 
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'UTC'
+    function createSeconds (seconds) {
+        const data = new Date (seconds * 1000); // A engine do JS sempre conta em Milisegundos 
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
     });
 }
 
 // Criar constante para selecionar o relogio/ iniciar / pausar / zerar 
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
+    const relogio = document.querySelector('.relogio');
 
-let seconds = 0; // Variável para manter um estado 
-let timer; // Variável para iniciar a função 
+    let seconds = 0; // Variável para manter um estado 
+    let timer; // Variável para iniciar a função 
 
 // Função para iniciar o relógio 
-function startClock() {
-    timer = setInterval(function() {
-        seconds++;
-        relogio.innerHTML = createSeconds(seconds);
+    function startClock() {
+        timer = setInterval(function() {
+            seconds++;
+            relogio.innerHTML = createSeconds(seconds);
     }, 1000);
 }
 
 // Chamada da função para iniciar o timer 
-iniciar.addEventListener('click', function(event){
-    clearInterval(timer);
-    startClock();
-})  
+    document.addEventListener('click', function(e){
+        const el = e.target;
 
-// Pausar o timer e zerar 
-pausar.addEventListener('click', function(event){
-    clearInterval(timer);
-});
+        if (el.classList.contains('iniciar')){
+            relogio.classList.remove('pausado');
+            clearInterval(timer);
+            startClock();
+    }
 
-zerar.addEventListener('click', function(event){
-    clearInterval(timer);
-    relogio.innerHTML = '00:00:00';
-    seconds = 0;
-});
+        if (el.classList.contains('pausar')){
+            clearInterval(timer);
+            relogio.classList.add('pausado');
+    }
+
+        if (el.classList.contains('zerar')){
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00';
+            relogio.classList.remove('pausado');
+            seconds = 0;
+    }
+
+
+})
+}
+clock();
